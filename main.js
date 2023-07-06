56e3401f7ab163c62d133f8c3c6291ade6aa7120
Yassinmoh <yassin_mohamed310@outlook.com>
2023-07-07 02:44:32 +0300

    add starter files

---
+++ main.js
@@ -0,0 +1,56 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const renderer = new THREE.WebGLRenderer();
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement)
+
+camera.position.z=3;
+camera.position.y=2;
+
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+orbit.update();
+
+const material =new THREE.MeshDepthMaterial()
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5,16,16),
+    material
+)
+
+sphere.position.x= -1.5
+
+scene.add(sphere)
+
+const clock =new THREE.Clock()
+function animate() {
+    const eltime =clock.getElapsedTime()
+
+    sphere.rotation.y= 0.1 *eltime
+    sphere.rotation.x= 0.15 *eltime
+    
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera);
+}
+animate()
+
+window.addEventListener('resize', function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+
+//handle full screen
+window.addEventListener("dblclick",()=>{
+    if(!document.fullscreenElement){
+        document.body.requestFullscreen()
+    }else{
+        document.exitFullscreen()
+    }
+})
